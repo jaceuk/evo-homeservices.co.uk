30e5447d5b191832eaa2b4b42d8d59b1928d7091
jaceuk <jnewington@gmail.com>
2023-07-26 18:02:07 +0100

    Build

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,21 +5,3 @@ export interface Review {
 	date: string;
 	postcode: string;
 }
-
-export interface Location {
-	id: number;
-	county: number;
-	name: string;
-	slug: string;
-	postcode: string;
-	tel: string | null;
-	info: string | null;
-}
-
-export interface Service {
-	id: number;
-	name: string;
-	slug: string;
-	templates: number;
-	website: string;
-}
